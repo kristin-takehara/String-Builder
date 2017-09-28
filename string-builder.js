0591facec12a630af376944eeabb9bdadff4bdde
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-09-27 14:37:00 -1000

    one test passing

---
+++ string-builder.js
@@ -0,0 +1,29 @@
+//jshint esversion: 6
+
+class StringBuilder {
+  // constructor(string) {
+  //   this.string = string;
+  // }
+
+  reverse() {
+    return this.reverse;
+  }
+
+  capitalize() {
+    return this.capitalize;
+  }
+
+  toArray() {
+    return this.toArray;
+  }
+
+  append() {
+    return this.append;
+  }
+
+  toString() {
+    return this.toString;
+  }
+}
+
+module.exports = StringBuilder;
--- test/test.js
+++ test/test.js
@@ -1,7 +1,23 @@
 //jshint esversion: 6
 
+
 const chai = require('chai');
 const expect = chai.expect;
 const should = chai.should();
 const StringBuilder = require('../string-builder');
+
+
+
+describe('StringBuilder', function() {
+
+  var SB;
+  beforeEach (function () {
+    SB = new StringBuilder('foo');
+  });
+
+  it('should be a string', function() {
+    StringBuilder.should.be.a.string;
+  });
+
+});
 
